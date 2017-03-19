6d21946269c10228fb6ef2084adc85edf09b3b0e
dpazj <joepaz200@gmail.com>
2017-03-19 21:02:08 +0000

    Ignore

--- Joes src/src/makelowercase.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-#the translation program made some translations into uppercase,
-#it is a good idea to ensure all strings are entirely lowercase
-filename="$1"
-while read -r line
-do
-	name=${line,,} 
-	echo $name >> $filename.change
-done < "$filename"
